2390f870f99e947987a3e5ad3b81dfac902e23d0
lokeshr <lokeshsaravanan.r@gmail.com>
2022-07-19 10:01:38 -0400

    New changes

--- lib/screens/unitselector.dart
+++ lib/screens/unitselector.dart
@@ -5,6 +5,48 @@ class UnitSelectorScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Select the unit to convert"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              print("add button pressed");
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.remove),
+            onPressed: () {
+              print("remove button pressed");
+            },
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              margin: EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.brown,
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(20.0)),
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Celcius to Fahrenheit",
+                  style: TextStyle(fontSize: 30.0),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
