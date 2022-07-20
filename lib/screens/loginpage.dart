3e88d870b3f918e8ce0c1c1859e8ee9f8241589d
lokeshr <lokeshsaravanan.r@gmail.com>
2022-07-20 10:50:31 -0400

    New changes

--- lib/screens/loginpage.dart
+++ lib/screens/loginpage.dart
@@ -1,3 +1,4 @@
+import 'package:anyconverter/data/userdata.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -10,11 +11,13 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  //We use text controller to get a value from the field. we have 2 fields.
   final userNameController = TextEditingController();
   final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    UserData.init();
     return Scaffold(
       body: SafeArea(
         child: Center(
@@ -35,12 +38,26 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                 ),
               ),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "Enter the username",
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Enter the username",
+                  ),
+                  controller: userNameController,
+                  textCapitalization: TextCapitalization.none,
                 ),
-                controller: userNameController,
-                textCapitalization: TextCapitalization.none,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  obscureText: true, //This is for not showing the password.
+                  decoration: InputDecoration(
+                    hintText: "Enter the password",
+                  ),
+                  controller: passwordController,
+                  textCapitalization: TextCapitalization.none,
+                ),
               )
             ],
           ),
@@ -49,17 +66,69 @@ class _LoginScreenState extends State<LoginScreen> {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.forward),
         onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                // Retrieve the text the that user has entered by using the
-                // TextEditingController.
-                title: Text("Welcome Message"),
-                content: Text(userNameController.text),
-              );
-            },
-          );
+          if (userNameController.text.isNotEmpty &&
+              passwordController.text.isNotEmpty) {
+            String message = "";
+
+            print(userNameController.text +
+                "   password is  " +
+                passwordController.text);
+
+            if (!UserData.userData.containsKey(userNameController.text)) {
+              message = "User not found !!!";
+            } else if (UserData.userData[userNameController.text]
+                    ?.compareTo(passwordController.text) ==
+                0) {
+              message = "Welcome to the app!!";
+            } else
+              message = "Incorrect Password";
+
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                    // Retrieve the text the that user has entered by using the
+                    // TextEditingController.
+
+                    title: Text(message),
+                    content: Text(userNameController.text),
+                    actions: [
+                      TextButton(
+                          child: Text("Ok"),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          })
+                    ]);
+              },
+            );
+          } else {
+            String message = "";
+            if (userNameController.text.isEmpty &&
+                passwordController.text.isEmpty)
+              message = "No input provided for username and password!!";
+            else if (userNameController.text.isEmpty)
+              message = "User name not found or Empty!!";
+            else
+              message = "Password not found or Empty!!";
+
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                    // Retrieve the text the that user has entered by using the
+                    // TextEditingController.
+
+                    title: Text(message),
+                    actions: [
+                      TextButton(
+                          child: Text("Ok"),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          })
+                    ]);
+              },
+            );
+          }
         },
       ),
     );
